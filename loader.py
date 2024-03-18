ddb60b27eab7b515fd4aae526b1ccae68a6941ca
oscar92i <oscar.migeon@epfedu.fr>
2024-03-18 21:48:35 +0100

    step 1 in loader

--- loader.py
+++ loader.py
@@ -3,7 +3,61 @@ import requests
 import numpy as np
 import pandas as pd
 
+
+DATA_PATH_STEP1 = 'sample_dirty.csv'
+
+def sample_formatted(data_csv):
+    # still need to choose which columns we have to keep
+    kept_columns = [
+        'nom', 'adr_num', 'adr_voie',
+        'com_cp', 'com_nom',
+        'tel1',
+        'freq_mnt', 'dermnt',
+        'lat_coor1', 'long_coor1']
+    df_local = pd.read_csv(data_csv)
+    df_local = pd.DataFrame(df_local, columns=kept_columns)
+    print(df_local)
+    return df_local
+
+
+def sample_sanitized(df_local):
+    # sanitizing nom
+    df['nom'] = df['nom'].str.replace('�', 'é')
+    print(df_local['nom'])
+
+    # sanitizing com_nom
+    df.loc[pd.to_numeric(df['com_nom'], errors='coerce').notna(), 'com_nom'] = 'Montpellier'
+    df['com_nom'] = df['com_nom'].replace(' ', 'Montpellier')
+    print(df_local['com_nom'])
+
+    # sanitizing adr_voie
+    df['adr_voie'] = df['adr_voie'].str.replace('�', 'é')
+    print(df_local['adr_voie'])
+
+    return df_local
+
+
+def sample_framed(df_local):
+    df_local.rename(columns={'dermnt': 'der_mnt', 'tel1': 'tel_num1'}, inplace=True)
+
+    return df_local
+
+
+df=sample_formatted(DATA_PATH_STEP1)
+df=sample_sanitized(df)
+df=sample_framed(df)
+
+
 DATA_PATH = 'data/MMM_MMM_DAE.csv'
+
+def sample_formatted(data_csv):
+    kept_columns = ['nom,lat_coor1,x,long_coor1,y,adr_num,adr_voie,com_cp,com_insee,com_nom,acc,acc_lib,acc_pcsec,acc_acc,acc_etg,acc_complt,photo1,photo2,disp_j,disp_h,disp_compl,tel1,tel2,site_email,date_insta,etat_fonct,fab_siren,fab_rais,mnt_siren,mnt_rais,modele,num_serie,id_euro,lc_ped,dtpr_lcped,dtpr_lcad,dtpr_bat,freq_mnt,dispsurv,dermnt,expt_siren,expt_rais,expt_tel1,expt_tel2,expt_email,ref,id,appartenan,dae_mobile,date1insta,chgtpiles']
+    pd.read_csv(data_csv)
+    print(data_csv)
+
+sample_formatted('sample_dirty.csv')
+
+
 
 def download_data(url, force_download=False, ):
     # Utility function to donwload data if it is not in disk
